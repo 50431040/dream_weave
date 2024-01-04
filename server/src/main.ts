3eec8acdab0cda9c2a9ab996ef71e492a062ca21
stahsf <50431040@qq.com>
2024-01-04 23:45:22 +0800

    feat: add helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
-import * as compression from 'compression';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.use(compression());
   app.use(cookieParser());
   app.enableCors();
