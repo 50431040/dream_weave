da782157018ec87d052ab0f7ec875b78be933d19
stahsf <50431040@qq.com>
2024-01-14 22:09:05 +0800

    feat: add right content

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,16 +1,22 @@
 // 运行时配置
 
+import { RunTimeLayoutConfig } from "@umijs/max";
+import RightContent from "./components/RightContent";
+
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<{ name: string }> {
   return { name: 'Dream Weave' };
 }
 
-export const layout = () => {
+export const layout: RunTimeLayoutConfig = () => {
   return {
     logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
     menu: {
       locale: false,
     },
+    rightRender: () => <RightContent />,
+    rightContentRender: () => <RightContent />,
+    layout: 'mix',
   };
 };
---
+++ web/src/components/RightContent/RightContent.tsx
@@ -0,0 +1,12 @@
+import { Layout, Row, Typography } from 'antd';
+import React from 'react';
+import styles from './Guide.less';
+
+// 脚手架示例组件
+const RightContent: React.FC = () => {
+  return (
+    <div>username</div>
+  );
+};
+
+export default RightContent;
