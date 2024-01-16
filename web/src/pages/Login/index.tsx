e5285d28cc3e890621d8db3ccda210cdda8f9e9d
stahsf <50431040@qq.com>
2024-01-16 23:54:01 +0800

    feat: login

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -3,12 +3,20 @@ import { history, useModel } from '@umijs/max';
 import React from 'react';
 import FormNormalLogin from './FormNormalLogin';
 import styles from './index.module.css';
+const CryptoJS = require('crypto-js');
 
 const Login: React.FC = () => {
   const { setUserInfo } = useModel('user');
 
   const handleLogin = async (form: any) => {
-    const res = await httpRequest.post('/login', form);
+    const password = CryptoJS.MD5(form.password);
+    const passwordMd5 = password.toString();
+
+    const res = await httpRequest.post('/user/login', {
+      username: form.username,
+      password: passwordMd5,
+    });
+
     // @ts-ignore
     setUserInfo(res);
     history.push('/index');
